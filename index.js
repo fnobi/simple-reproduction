e66faecf119d0be7b81e1ee9849af8d80595f655
nobii <fujisawa-shin@kayac.com>
2018-08-23 22:25:25 +0900

    コードをとってくる

---
+++ index.js
@@ -0,0 +1,55 @@
+const EventEmitter = require("events").EventEmitter;
+const path = require("path");
+
+const _ = require("lodash");
+const fs = require("mz/fs");
+const mkdirp = require("mkdirp-promise");
+const cheerio = require("cheerio");
+
+class SimpleReproduction extends EventEmitter {
+  start({ src, routes }) {
+    return Promise.resolve()
+      .then(() => fs.readFile(src, "utf-8"))
+      .then(html => {
+        return Promise.all(
+          _.map(routes, (route, dest) => {
+            const htmlDest = completeHtmlPath(dest);
+            return Promise.resolve()
+              .then(() => mkdirp(htmlDest.replace(/[^/]+\.html$/, "")))
+              .then(() => fs.writeFile(htmlDest, applyRouteOption(html, route)))
+              .then(() =>
+                this.emit("write", {
+                  htmlDest,
+                  route
+                })
+              );
+          })
+        );
+      })
+      .then(() => this.emit("end"));
+  }
+}
+
+function completeHtmlPath(pathName) {
+  return /\.html$/.test(pathName)
+    ? pathName
+    : path.join(pathName, "index.html");
+}
+
+function applyRouteOption(html, route) {
+  const $ = cheerio.load(html);
+  if (route.title) {
+    $("title").text(route.title);
+  }
+  if (route.meta) {
+    _.each(route.meta, (content, name) => {
+      const $meta = $(
+        [`meta[name="${name}"]`, `meta[property="${name}"]`].join(",")
+      );
+      $meta.attr("content", content);
+    });
+  }
+  return $.html();
+}
+
+module.exports = SimpleReproduction;
